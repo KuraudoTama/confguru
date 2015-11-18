b8f0f414270b799dbff91e4cf8d209a745291fd0
Di Xu <dixudx@cn.ibm.com>
2015-11-18 23:06:48 +0800

    add backend cls for zookeeper

--- confguru/guru.py
+++ confguru/guru.py
@@ -1,5 +1,10 @@
+from confguru.backends import ZKClient
+
+
 class Guru(object):
     """A Common Client to Interact with Various Configuration Endpoints
     """
 
-    pass
+    def __init__(self, backend_type, *args, **kwargs):
+        if backend_type == "zookeeper":
+            self = ZKClient(*args, **kwargs)
